e4843cf5f4b9b34c38fffae0de494310dec6046b
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2019-11-15 10:42:05 -0500

    cleaned up code

--- components/upload page/image.php
+++ components/upload page/image.php
@@ -1,158 +1,152 @@
 <!DOCTYPE html>
 <head>
-   <style>
-      @import url("style.css"); /* Using a url */
-   </style>
-   <meta charset="utf-8">
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-   <script>
-   <?php
-      $host="127.0.0.1";
-      $port=3306;
-      $socket="";
-      $user="c2375a05";
-      $password="!c2375aU!";
-      $dbname="c2375a05test";
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<script>
+<?php
 
-      $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-        or die ('Could not connect to the database server' . mysqli_connect_error());
+  $host="127.0.0.1";
+  $port=3306;
+  $socket="";
+  $user="c2375a05";
+  $password="!c2375aU!";
+  $dbname="c2375a05test";
 
-      $username=$_COOKIE["username"];
+  $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+    or die ('Could not connect to the database server' . mysqli_connect_error());
 
-      $categories = array();
-      $choices = array();
+  $username=$_COOKIE["username"];
 
-      // sql query to build menu
-      $query = "select id, category, choice from my_options where lower(username) = lower('".$username."')";
+  $categories = array();
+  $choices = array();
 
-      if ($stmt = $con->prepare($query)) {
-          $stmt->execute();
-          $stmt->bind_result($id, $category, $choice);
-          while ($stmt->fetch()) {
+  // sql query to build menu
+  $query = "select id, category, choice from my_options where lower(username) = lower('".$username."')";
 
-            $categories[$id]=$category;
+  if ($stmt = $con->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($id, $category, $choice);
+    while ($stmt->fetch()) {
 
-            if(!isset($choices[$id])) {
+      $categories[$id]=$category;
 
-              $choices[$id] = array();
+      if(!isset($choices[$id])) {
 
-            }
+        $choices[$id] = array();
 
-            if($choice)
-              array_push($choices[$id], $choice);
-
-          }
-          $stmt->close();
       }
 
-      echo "var my_categories = JSON.parse('" . json_encode($categories) . "');";
-      echo "var my_choices = JSON.parse('" . json_encode($choices) . "');";
+      if($choice)
+        array_push($choices[$id], $choice);
 
+    }
+    $stmt->close();
+  }
 
-   ?>
-   </script>
+  echo "var my_name = '" . $username . "';\n";
+  echo "var my_categories = JSON.parse('" . json_encode($categories) . "');\n";
+  echo "var my_choices = JSON.parse('" . json_encode($choices) . "');\n";
+
+?>
+</script>
 </head>
 <body>
-   <form action="uploadcss.php" method="post" enctype="multipart/form-data">
-      <div class="wrapper">
-         <header>
-            <h1>Upload Images</h1>
-            <span></span>
-         </header>
-         <div class="sections">
-            <section class="active">
-               <input type="text" placeholder="Image Title" id="title" name="imageName"/>
-               <select id="category" onchange="change_choices(this.value)" name="category">
-               </select>
-               <div id="choice" class="dropdown" name="choice">
-                  <div class="dropbtn">Choices</div>
-                  <div class="dropdown-content" id="choices" name="choices"></div>
-                </div>
-                
-   <div class="image-upload-wrap">
-     <input class="file-upload-input" type='file' onchange="readURL(this);" accept="image/png,image/gif,image/jpeg,image/webp" id="fileToUpload" name="fileToUpload"/>
+<form action="uploadcss.php" method="post" enctype="multipart/form-data">
+  <div class="wrapper">
+    <header>
+      <h1>Upload Image</h1>
+    </header>
+    <div class="sections">
 
-     <div class="drag-text">
-      <img id="preview" alt="DRAG AND DROP OR CLICK TO BROWSE">
-     </div>
-     
-   </div>
-   
-   </section>
+      <section class="active">
+        <select id="category" onchange="update_choices()" name="category"></select>
+        <div id="choice" class="dropdown" name="choice">
+          <div class="dropbtn">Choices</div>
+          <div class="dropdown-content" id="choices" name="choices"></div>
+        </div>
 
-   <section>
-   <input type="text" placeholder="Topic" id="topic"/>
-   <textarea placeholder="something..." id="msg"></textarea>
-   </section>
-   </div>
-   <footer>
-   <ul>
-   <input class="button buttonReset" type="reset" value="Reset">
-   <input class="button buttonReset" type="submit" value="Submit">
-   </ul>
-   </footer>
-   </div>
-   </form>
-   <div class="notification"></div>
-   <footer></footer>
-   <script>
+        <div class="image-upload-wrap">
 
-      var category = document.getElementById("category");
+          <input class="file-upload-input" type='file' onchange="readURL(this);" accept="image/png,image/gif,image/jpeg,image/webp" id="fileToUpload" name="fileToUpload">
 
-      var choices = document.getElementById("choices");
+          <div class="drag-text">
+            <img id="preview" alt="DRAG AND DROP OR CLICK TO BROWSE">
+          </div>
 
-      var select = document.getElementById("category");
+        </div>
+      </section>
 
+      <section>
+        <input type="text" placeholder="Topic" id="topic"/>
+        <textarea placeholder="something..." id="msg"></textarea>
+      </section>
 
-      for(var i = 0; i < my_categories.length; i++) {
-          var opt = my_categories[i];
-          var el = document.createElement("option");
-          el.textContent = opt;
-          el.value = opt;
-          select.appendChild(el);
-      }
+    </div>
 
-      function change_choices(value)
-      {
+    <footer>
+      <ul>
+      <input class="button buttonReset" type="reset" value="Reset">
+      <input class="button buttonReset" type="submit" value="Submit">
+      </ul>
+    </footer>
+  </div>
+</form>
 
-      var myDiv = document.getElementById("choices");
+<script>
+var category = document.getElementById("category");
+var choices = document.getElementById("choices");
+var select = document.getElementById("category");
 
-      myDiv.innerHTML = "";
+var category_count = my_categories.length;
 
-      for(var i = 0; i < my_choices[category.selectedIndex].length; i++) {
-          var checkBox = document.createElement("input");
-          var a = document.createElement("a");
-          checkBox.type = "checkbox";
-          checkBox.value = my_choices[category.selectedIndex][i];
-          checkBox.name = "choice[]";
-          a.appendChild(checkBox);
-          myDiv.appendChild(a);
-          a.appendChild(document.createTextNode(my_choices[category.selectedIndex][i]));
-      	}
-      }
+for(var i = 0; i < category_count; i++)
+{
+  var opt = my_categories[i];
+  var el = document.createElement("option");
+  el.textContent = opt;
+  el.value = opt;
+  select.appendChild(el);
+}
 
-      category.onchange();
+function update_choices()
+{
 
-      function readURL(input) {
-  if (input.files && input.files[0]) {
+  choices.innerHTML = "";
+  var choice_count = my_choices[category.selectedIndex].length;
+
+  for(var i = 0; i < choice_count; i++)
+  {
+    var checkBox = document.createElement("input");
+    var a = document.createElement("a");
+    checkBox.type = "checkbox";
+    checkBox.value = my_choices[category.selectedIndex][i];
+    checkBox.name = "choice[]";
+    a.appendChild(checkBox);
+    choices.appendChild(a);
+    a.appendChild(document.createTextNode(my_choices[category.selectedIndex][i]));
+  }
+
+}
+
+update_choices();
+
+function readURL(input)
+{
+  if (input.files && input.files[0])
+  {
     var reader = new FileReader();
 
-    reader.onload = function(e) {
-      $('#preview').attr('src', e.target.result);
+    reader.onload = function(e)
+    {
+      document.getElementById("preview").src = e.target.result;
     }
 
     reader.readAsDataURL(input.files[0]);
     document.getElementById("test1").innerHTML = document.getElementById("fileToUpload").value.split(/(\\|\/)/g).pop();
 
-
   }
 }
+</script>
 
-$("#imgInp").change(function() {
-  readURL(this);
-});
-
-
-   </script>
 </body>
 </html>
