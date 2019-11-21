24961bf88693270966822c1e2b5de60a907829cc
Matthew Below <55146074+mjbelow@users.noreply.github.com>
2019-11-20 19:08:39 -0500

    implemented feature to delete or modify images

--- components/image page/index.php
+++ components/image page/index.php
@@ -7,9 +7,65 @@
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <style type="text/css">
 body {
-background: none;
+  background: none;
+}
+
+.category, .dropdown {
+  width: 100%;
+  margin-bottom: 4px;
+}
+
+.category, .dropbtn {
+  background-color: white;
+  color: #555555;
+  padding: 5px;
+  font-size: 16px;
+  border: 2px solid #555555;
+  cursor: pointer;
+}
+
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: white;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+.dropdown-content label {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+
+.dropdown-content label:hover {
+  background-color: #555555;
+  color: #eee;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #555555;
+  color: white;
+}
+
+.buttons {
+  text-align: center;
 }
 </style>
+<script type="application/javascript" src="js/jquery.min.js"></script>
 <script type="application/javascript">
 <?php
 $host="127.0.0.1";
@@ -36,9 +92,9 @@ $images = array();
 // sql query
 //$query = "select category.name category, image.name image from c2375a05proj.image   inner join c2375a05proj.category     on image.category_id = category.id where   (category_id = 1 and choices in (1,2,3)) order by image.category_id, image.choices";
 //$query = "select category.name category, image.name image from c2375a05proj.image   inner join c2375a05proj.category     on image.category_id = category.id where   (category_id = 1 and choices in (1,2,3))   or   (category_id = 2 and choices in (32,128,64,512)) order by image.category_id, image.choices";
-$query = "select category.name category, image.name image " .
+$query = "select category.name category, image.name image, image.category_id, image.choice_array " .
 "from " .
-  "(select name, category_id, sum(pow(2, choice_id-1)) choices, username " .
+  "(select name, category_id, sum(pow(2, choice_id-1)) choices, username, group_concat(choice_id) choice_array " .
     "from image " .
     "group by username, category_id, name " .
     "order by username, category_id, choices, name) image " .
@@ -50,7 +106,7 @@ $output = $_GET['output'];
 $category_count = count($output);
 for($i = 0; $i < $category_count; $i++)
 {
-  
+
   $category_item = (explode(",", $output[$i]));
   $item_count = count($category_item);
 
@@ -71,7 +127,7 @@ for($i = 0; $i < $category_count; $i++)
     "order by category_id, choices, image.name";
   else
     $query .= ")) or ";
-  
+
 }
 echo "//" . $query . "\n";
 
@@ -80,36 +136,62 @@ echo "//" . $query . "\n";
 
 if ($stmt = $con->prepare($query)) {
     $stmt->execute();
-    $stmt->bind_result($category, $image);
+    $stmt->bind_result($category, $image, $category_id, $choice_array);
     while ($stmt->fetch()) {
-      
+
       // if key doesn't exist for a category, assign one a new array and add to categories array
       if(!isset($images[$category])) {
-        
+
         $images[$category] = array();
         array_push($categories, $category);
-        
+
       }
-      
-      array_push($images[$category], $image);
-      
+
+      array_push($images[$category], array($image, $category_id, $choice_array));
+
     }
     $stmt->close();
 }
 
 
+  $my_categories = array();
+  $my_choices = array();
+
+  // sql query to build menu
+  $query = "select id, category, choice from my_options where lower(username) = lower('".$username."')";
+
+  if ($stmt = $con->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($id, $category, $choice);
+    while ($stmt->fetch()) {
+
+      $my_categories[$id]=$category;
+
+      if(!isset($my_choices[$id])) {
+
+        $my_choices[$id] = array();
+
+      }
+
+      if($choice)
+        array_push($my_choices[$id], $choice);
+
+    }
+    $stmt->close();
+  }
+
 $con->close();
 
-// echo json_encode($images);
-// echo "<br>";
-// echo json_encode($categories);
 
-echo "var images = JSON.parse('" . json_encode($images) . "');";
-echo "\n";
-echo "var categories = " . json_encode($categories) . ";";
+
+echo "var my_categories = JSON.parse('" . json_encode($my_categories) . "');\n";
+echo "var my_choices = JSON.parse('" . json_encode($my_choices) . "');\n";
+
+echo "var images = JSON.parse('" . json_encode($images) . "');\n";
+echo "var categories = " . json_encode($categories) . ";\n";
 
 ?>
-
+var category_count = my_categories.length;
 </script>
 </head>
 
@@ -145,6 +227,9 @@ for(i = 0; i < count; i++)
 
   for(j = 0; j < image_count; j++)
   {
+    var category_index = images[group][j][1] - 1;
+    var choices_array = (images[group][j][2]).split(",");
+
     var img_container = document.createElement("div");
     img_container.className="img_container";
 
@@ -155,7 +240,9 @@ for(i = 0; i < count; i++)
     img_info.title="view / modify image data";
 
     var img = document.createElement("img");
-    img.src = "images/" + images[group][j];
+    img.src = "images/" + images[group][j][0];
+    img.dataset.category=category_index;
+    img.dataset.choices=choices_array;
     img.onclick=enlarge(img.src);
 
     img.alt = "image"+pad(j+1);
@@ -166,14 +253,171 @@ for(i = 0; i < count; i++)
 
 
 
+    var info = document.createElement("div");
+    info.className="wrapper";
+    info.style.display="none";
 
-    img_info.onclick = img_meta(img);
+    var category_select = document.createElement("select");
+    category_select.className="category";
+
+    for(var k = 0; k < category_count; k++)
+    {
+
+      var option = document.createElement("option");
+      option.innerHTML=my_categories[k];
+      option.value=my_categories[k];
+      category_select.appendChild(option);
+    }
+
+    category_select.selectedIndex=category_index;
+
+
+    var dropdown = document.createElement("div");
+    dropdown.className="dropdown";
+    dropdown.innerHTML="<div class='dropbtn'>Choices</div>";
+
+    var choices = document.createElement("div");
+    choices.className="dropdown-content";
+
+    var choices_count = my_choices[category_index].length;
+
+    for(var k = 0; k < choices_count; k++)
+    {
+      var label = document.createElement("label");
+      label.setAttribute("for", images[group][j][0] + k);
+      var checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+      checkBox.value = my_choices[category_select.selectedIndex][k];
+      checkBox.name = "choice[]";
+      checkBox.id=images[group][j][0] + k;
+      if((k+1) == choices_array[0])
+      {
+        checkBox.checked=true;
+        choices_array.shift();
+      }
+      label.appendChild(checkBox);
+      label.appendChild(document.createTextNode(my_choices[category_select.selectedIndex][k]));
+      choices.appendChild(label);
+    }
+
+    dropdown.appendChild(choices);
+
+    $(category_select).change(update_choices(choices, images[group][j][0]));
+
+    info.appendChild(category_select);
+    info.appendChild(dropdown);
+
+
+    $(info).append
+    (
+
+      $("<div>").addClass("buttons")
+        .append($("<button>").html("Delete").click(delete_img(img_container, category_select, images[group][j][0])))
+        .append($("<button>").html("Modify").click(modify_img(img_container, category_select, choices, images[group][j][0])))
+
+    );
+
+/*
+<div class="wrapper">
+
+  <select id="category" onchange="update_choices()" name="category"><option value="Tennis">Tennis</option><option value="Animals">Animals</option></select>
+
+  <div id="choice" class="dropdown" name="choice">
+
+    <div class="dropbtn">Choices</div>
+
+    <div class="dropdown-content" id="choices" name="choices">
+      <label for="1"><a><input type="checkbox" value="Rafael Nadal" name="choice[]" id="1">Rafael Nadal</a></label>
+    </div>
+
+  </div>
+
+</div>
+*/
+
+    img_container.appendChild(info);
+
+    img_info.onclick = img_meta(img, info);
+    //$(img_info).click(alrt(j))
 
 
     div.appendChild(img_container);
 
   }
 
+}
+
+function update_choices(choices, img)
+{
+  return function()
+  {
+    
+    var choices_count = my_choices[this.selectedIndex].length;
+    
+    choices.innerHTML="";
+    for(var i = 0; i < choices_count; i++)
+    {
+      var label = document.createElement("label");
+      label.setAttribute("for", img + i);
+      var checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+      checkBox.value = my_choices[this.selectedIndex][i];
+      checkBox.name = "choice[]";
+      checkBox.id=img + i;
+      label.appendChild(checkBox);
+      label.appendChild(document.createTextNode(my_choices[this.selectedIndex][i]));
+      choices.appendChild(label);
+    }
+    
+  }
+}
+
+function delete_img(container, category, img_src)
+{
+  return function()
+  {
+    $.post("modify.php", {category: category.value, img_src: img_src}, function(success)
+    {
+      if(success)
+      {
+        parent.start(5, null, null);
+        $(container).remove();
+      }
+    })
+  }
+}
+
+
+function modify_img(container, category, choices, img_src)
+{
+  return function()
+  {
+    var choice_array = [];
+    $(choices).find("input:checked").each(function()
+      {
+        choice_array.push($(this).val());
+      }
+    );
+    
+    $.post("modify.php", {category: category.value, choice_array: JSON.stringify(choice_array), img_src: img_src},
+    function(success)
+    {
+      if(success)
+      {
+        parent.start(6, null, null);
+        $(container).css("border-color", "rgb(116, 163, 255)");
+      }
+    });
+    
+  }
+}
+
+function alrt(j)
+{
+  return function()
+  {
+    alert(j);
+  }
 }
 
 function pad(n)
@@ -212,12 +456,13 @@ function enlarge(src)
 
 }
 
-function img_meta(img)
+function img_meta(img, info)
 {
 
   return function()
   {
-    alert(img.alt);
+    //info.style.display=info.style.display=="none"?"block":"none";
+    $(info).toggle();
   }
 
 }
---
+++ modify.php
@@ -0,0 +1,83 @@
+<?php
+$host="127.0.0.1";
+$port=3306;
+$socket="";
+$user="c2375a05";
+$password="!c2375aU!";
+$dbname="c2375a05proj";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+$username=$_COOKIE["username"];
+$category = $_POST["category"];
+$img = $_POST["img_src"];
+
+//mysqli_begin_transaction($con);
+mysqli_autocommit($con, false);
+
+$query = "delete from image where name='".$img."' and lower(username)=lower('".$username."')";
+if($con->query($query))
+{
+  if(isset($_POST["choice_array"]))
+  {
+
+    $choice = json_decode($_POST["choice_array"]);
+    $choice_count = count($choice);
+
+    $query = "select '" . $img . "', category.id, choice.id, category.username " .
+    "from category left join choice on ((category.id = choice.category_id) and (category.username = choice.username)) " .
+    "where (lower(category.username) = lower('".$username."')) and (lower(category.name) = lower('".$category."')) and (";
+
+    // build query using choices
+    for($i = 0; $i < $choice_count; $i++)
+    {
+      if($i == ($choice_count - 1))
+        $query .= "lower(choice.name) = lower('".$choice[$i]."')) and (";
+      else
+        $query .= "lower(choice.name) = lower('".$choice[$i]."') or ";
+    }
+
+    $query .= "true)";
+
+    if($con->query("insert into image (".$query.")"))
+    {
+      echo true;
+      // Commit transaction
+      mysqli_commit($con);
+    }
+    else
+    {
+      echo false;
+      // Rollback transaction
+      mysqli_rollback($con);
+    }
+
+  }
+
+  else
+  {
+
+    if(unlink("images/" . $_POST["img_src"]))
+    {
+      echo true;
+      // Commit transaction
+      mysqli_commit($con);
+    }
+    else
+    {
+      echo false;
+      // Rollback transaction
+      mysqli_rollback($con);
+    }
+    
+  }
+
+}
+else
+  echo false;
+
+$con->close();
+
+
+?>
